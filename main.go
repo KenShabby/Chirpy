9d5d8e4970e72f48a56012cf9b18a236588b80b6
Ken Shabby <kshabby@sonic.net>
2026-01-21 11:48:30 -0800

    add health response handler

--- main.go
+++ main.go
@@ -6,18 +6,24 @@ import (
 )
 
 func main() {
-	const port = "8081"
+	const port = "8080"
+	const filepathRoot = "."
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/healthz", handlerReadiness)
 
 	svr := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
 
-	err := svr.ListenAndServe()
-	if err != nil {
-		log.Fatal(err)
-	}
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(svr.ListenAndServe())
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
