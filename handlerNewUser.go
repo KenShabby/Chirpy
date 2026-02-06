a2a4059acf9b3e242b757a59a7eb27dd33615751
Ken Shabby <kshabby@sonic.net>
2026-02-05 19:52:37 -0800

    add user functionality implemented

--- handlerNewUser.go
+++ handlerNewUser.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"net/http"
 	"time"
 
 	"github.com/google/uuid"
@@ -13,5 +15,34 @@ type User struct {
 	Email     string    `json:"email"`
 }
 
-func handlerNewUser(email string) {
+func (cfg *apiConfig) handlerNewUser(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Email string `json:"email"`
+	}
+	type response struct {
+		User
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters", err)
+		return
+	}
+
+	user, err := cfg.db.CreateUser(r.Context(), params.Email)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't create user", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusCreated, response{
+		User: User{
+			ID:        user.ID,
+			CreatedAt: user.CreatedAt,
+			UpdatedAt: user.UpdatedAt,
+			Email:     user.Email,
+		},
+	})
 }
